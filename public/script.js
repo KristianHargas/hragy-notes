022de144fe99b8db3e0376bed578ff7d46ba288b
Kristián <kristian.hargas@gmail.com>
2020-10-24 19:31:15 +0200

    Responsive navbar added, some design issues fixed.

---
+++ public/script.js
@@ -0,0 +1,15 @@
+let isMobileMenuOpen = false
+
+const mobileMenu = document.getElementById('mobile_menu')
+const hamburgerBtn = document.getElementById('hamburger_btn')
+const hamburgerIcon = document.getElementById('hamburger_icon')
+const closeMenuIcon = document.getElementById('close_menu_icon')
+
+hamburgerBtn.addEventListener('click', function () {
+  isMobileMenuOpen = !isMobileMenuOpen
+  mobileMenu.classList.toggle('hidden')
+
+  hamburgerIcon.classList.toggle('hidden')
+  closeMenuIcon.classList.toggle('hidden')
+})
+
