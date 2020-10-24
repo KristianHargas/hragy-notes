909ac423a59e9bb8f65c128c1dd83e3674d35f7e
Kristián <kristian.hargas@gmail.com>
2020-10-24 23:43:43 +0200

    Sticky toolbar added, hamburger button transition added.

--- public/script.js
+++ public/script.js
@@ -8,6 +8,7 @@ const closeMenuIcon = document.getElementById('close_menu_icon')
 hamburgerBtn.addEventListener('click', function () {
   isMobileMenuOpen = !isMobileMenuOpen
   mobileMenu.classList.toggle('hidden')
+  hamburgerBtn.classList.toggle('open')
 
   hamburgerIcon.classList.toggle('hidden')
   closeMenuIcon.classList.toggle('hidden')
