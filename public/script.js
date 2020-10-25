8a15fa3b09605f47fed270c66a2120b4d1d0d879
Kristián <kristian.hargas@gmail.com>
2020-10-25 11:04:11 +0100

    NPM commands updated, footer added, mobile selection effects removed.

--- public/script.js
+++ public/script.js
@@ -1,16 +1,11 @@
 let isMobileMenuOpen = false
 
-const mobileMenu = document.getElementById('mobile_menu')
+const mobileMenu = document.getElementById('nav_menu')
 const hamburgerBtn = document.getElementById('hamburger_btn')
-const hamburgerIcon = document.getElementById('hamburger_icon')
-const closeMenuIcon = document.getElementById('close_menu_icon')
 
 hamburgerBtn.addEventListener('click', function () {
   isMobileMenuOpen = !isMobileMenuOpen
   mobileMenu.classList.toggle('hidden')
   hamburgerBtn.classList.toggle('open')
-
-  hamburgerIcon.classList.toggle('hidden')
-  closeMenuIcon.classList.toggle('hidden')
 })
 
